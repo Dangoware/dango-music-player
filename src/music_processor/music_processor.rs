9670ff0a7265c3430a515b282134401735af10ae
walter <walterofnone@protonmail.com>
2023-08-16 21:59:01 -0500

    added missing folder

---
+++ src/music_processor/music_processor.rs
@@ -0,0 +1,30 @@
+use symphonia::core::audio::{AudioBuffer, AudioBufferRef, Signal, AsAudioBufferRef, SignalSpec};
+
+#[derive(Clone)]
+pub struct MusicProcessor {
+    pub audio_buffer: AudioBuffer<f32>,
+    pub audio_volume: f32,
+}
+
+impl MusicProcessor {
+    pub fn new() -> Self {
+        MusicProcessor {
+            audio_buffer: AudioBuffer::unused(),
+            audio_volume: 1.0,
+        }
+    }
+    
+    pub fn process(&mut self, audio_buffer_ref: &AudioBufferRef) -> AudioBufferRef {
+        audio_buffer_ref.convert(&mut self.audio_buffer);
+        
+        let process = |sample| sample * self.audio_volume;
+        
+        self.audio_buffer.transform(process);
+        
+        return self.audio_buffer.as_audio_buffer_ref();
+    }
+    
+    pub fn set_buffer(&mut self, duration: u64, spec: SignalSpec) {
+        self.audio_buffer = AudioBuffer::new(duration, spec);
+    }
+}
