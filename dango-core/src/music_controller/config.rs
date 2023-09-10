e64137bedf4dcf8841d9c06bbb044f3aa9108fd6
walter <walterofnone@protonmail.com>
2023-09-10 14:45:32 -0500

    Added preliminary Discord RPC support, updated configuration and error handling for music trackers. Closes #2

--- dango-core/src/music_controller/config.rs
+++ dango-core/src/music_controller/config.rs
@@ -4,23 +4,43 @@ use std::fs;
 
 use serde::{Deserialize, Serialize};
 
-use crate::music_tracker::music_tracker::LastFM;
+use crate::music_tracker::music_tracker::{LastFM, LastFMConfig, DiscordRPCConfig};
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
     pub db_path: Box<PathBuf>,
-    pub lastfm: Option<LastFM>,
+    pub lastfm: Option<LastFMConfig>,
+    pub discord: Option<DiscordRPCConfig>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        let path = PathBuf::from("./music_database.db3");
+
+        return Config {
+            db_path: Box::new(path),
+
+            lastfm: Some (LastFMConfig {
+                enabled: true,
+                dango_api_key: String::from("29a071e3113ab8ed36f069a2d3e20593"),
+                auth_token: None,
+                shared_secret: Some(String::from("5400c554430de5c5002d5e4bcc295b3d")),
+                session_key: None,
+            }),
+
+            discord: Some(DiscordRPCConfig {
+                enabled: true,
+                dango_client_id: 1144475145864499240,
+                dango_icon: String::from("flat"),
+            }),
+        };
+    }
 }
 
 impl Config {
     // Creates and saves a new config with default values
-    pub fn new(config_file: &PathBuf) -> std::io::Result<Config> {
-        let path = PathBuf::from("./music_database.db3");
-        
-        let config = Config {
-            db_path: Box::new(path),
-            lastfm: None,
-        };
+    pub fn new(config_file: &PathBuf) -> std::io::Result<Config> {        
+        let config = Config::default();
         config.save(config_file)?;
         
         Ok(config)
--- dango-music-player/src/main.rs
+++ dango-music-player/src/main.rs
@@ -1,3 +1,49 @@
+use std::{thread, path::PathBuf};
+
+use dango_core::{music_tracker::music_tracker::{DiscordRPC, DiscordRPCConfig, MusicTracker, LastFMConfig, LastFM}, music_controller::music_controller::MusicController};
+use async_std::{fs::File, io, prelude::*, task};
+
 fn main() {
-    println!("Hello, world!");
+    let ctl = MusicController::new(&PathBuf::from("config.toml")).unwrap();
+
+    let conf = DiscordRPCConfig {
+        enabled: true,
+        dango_client_id: 1144475145864499240,
+        dango_icon: String::from("flat"),
+    };
+    let mut disc: Box<dyn MusicTracker> = Box::new(DiscordRPC::new(&conf));
+    let song = String::from("Listening to ASM");
+    let good = task::block_on(async {
+        return disc.track_now(&song).await
+    });
+    println!("good: {:?}", good);
+    
+    let fm = LastFMConfig {
+        enabled: true,
+                dango_api_key: String::from("29a071e3113ab8ed36f069a2d3e20593"),
+                auth_token: None,
+                shared_secret: Some(String::from("5400c554430de5c5002d5e4bcc295b3d")),
+                session_key: None,
+    };
+    
+    let mut last = LastFM::new(&fm);
+    
+    let url = task::block_on(async {
+        last.get_auth_url().await.unwrap()
+    });
+    
+    println!("URL: {url}");
+    
+    thread::sleep_ms(10000); 
+    
+    let sk = task::block_on(async {
+        last.set_session().await
+    });
+    
+    let song = String::from("song");
+    let track = task::block_on(async {
+        last.track_now(&song).await
+    });
+    
+    thread::sleep_ms(5000000);
 }
