692e3770d1c827365b4122ca4ac15eef5ac644f1
G2-Games <ke0bhogsg@gmail.com>
2023-11-01 10:57:15 -0500

    Finished CUE file support

--- dango-music-player/src/main.rs
+++ dango-music-player/src/main.rs
@@ -9,12 +9,12 @@ fn main() {
     let config = Arc::new(RwLock::new(Config::default()));
     let mut library = MusicLibrary::init(config.clone()).unwrap();
 
-    /*
+
     let now = std::time::Instant::now();
+    //let total = library.find_all_music("/home/g2/Music/Random Songs/KICM-3158.cue", &config.clone().read().unwrap()).unwrap();
     let total = library.find_all_music("/home/g2/Downloads/Albums", &config.clone().read().unwrap()).unwrap();
     let time = now.elapsed().as_micros() as f32 / 1000.0;
     println!("{} songs in {}ms", total, time);
-    */
 
     let query_res = library.query(
         &String::from("みなみけ きゃらくた"),
@@ -24,6 +24,6 @@ fn main() {
     ).unwrap();
 
     for result in query_res {
-        println!("Start/End: {:?}/{:?}\nTitle: {}\nAlbum: {}\n", result.location.start().unwrap_or(&std::time::Duration::from_secs(0)), result.location.end().unwrap_or(&std::time::Duration::from_secs(0)), result.get_tag(&Tag::Title).unwrap(), result.get_tag(&Tag::Album).unwrap());
+        println!("Start/End: {:?}/{:?}\nTitle: {}\nAlbum: {}\n", result.location.start().unwrap_or(&std::time::Duration::from_secs(0)), result.location.end().unwrap_or(&std::time::Duration::from_secs(0)), result.get_tag(&Tag::Title).unwrap(), result.get_tag(&Tag::Album).unwrap_or(&"".to_string()));
     }
 }
