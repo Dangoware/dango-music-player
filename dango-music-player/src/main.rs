4d50468d1f3b728f124ba90171237496c1213657
G2-Games <ke0bhogsg@gmail.com>
2023-11-01 14:13:25 -0500

    Fixed duplicate entries

--- dango-music-player/src/main.rs
+++ dango-music-player/src/main.rs
@@ -13,17 +13,24 @@ fn main() {
     let now = std::time::Instant::now();
     //let total = library.find_all_music("/home/g2/Music/Random Songs/KICM-3158.cue", &config.clone().read().unwrap()).unwrap();
     let total = library.find_all_music("/home/g2/Downloads/Albums", &config.clone().read().unwrap()).unwrap();
+    //let total = library.find_all_music("/home/g2/Music/Albums/", &config.clone().read().unwrap()).unwrap();
     let time = now.elapsed().as_micros() as f32 / 1000.0;
     println!("{} songs in {}ms", total, time);
 
     let query_res = library.query(
-        &String::from("みなみけ きゃらくた"),
-        &vec![Tag::Album],
-        false,
+        &String::from("KICM"),
+        &vec![Tag::Title],
+        true,
         &vec![Tag::Field(String::from("location"))]
     ).unwrap();
 
     for result in query_res {
-        println!("Start/End: {:?}/{:?}\nTitle: {}\nAlbum: {}\n", result.location.start().unwrap_or(&std::time::Duration::from_secs(0)), result.location.end().unwrap_or(&std::time::Duration::from_secs(0)), result.get_tag(&Tag::Title).unwrap(), result.get_tag(&Tag::Album).unwrap_or(&"".to_string()));
+        println!("Location: {}\nStart/End: {:?}/{:?}\nTitle: {}\nAlbum: {}\n",
+            result.location.path_string(),
+            result.location.start().unwrap_or(&std::time::Duration::from_secs(0)),
+            result.location.end().unwrap_or(&std::time::Duration::from_secs(0)),
+            result.get_tag(&Tag::Title).unwrap_or(&"".to_string()),
+            result.get_tag(&Tag::Album).unwrap_or(&"".to_string())
+        );
     }
 }
