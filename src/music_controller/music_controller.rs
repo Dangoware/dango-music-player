eeceb27800675913a263c8807b393350ea26992b
G2-Games <ke0bhogsg@gmail.com>
2023-11-03 08:39:19 -0500

    Added album query function

--- src/music_controller/music_controller.rs
+++ src/music_controller/music_controller.rs
@@ -85,6 +85,6 @@ impl MusicController {
         sort_by: Vec<Tag>,
     ) -> Option<Vec<&Song>> {
         self.library
-            .query(query_string, &target_tags, search_location, &sort_by)
+            .query(query_string, &target_tags, &sort_by)
     }
 }
