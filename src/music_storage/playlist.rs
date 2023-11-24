ba4ed346ce018a7dd808ce4837aea8d95891edc2
G2-Games <ke0bhogsg@gmail.com>
2023-11-24 14:22:20 -0600

    Removed music_player in preparation for GStreamer based backend

--- src/music_storage/playlist.rs
+++ src/music_storage/playlist.rs
@@ -1,6 +1,6 @@
 use crate::music_controller::config::Config;
 use std::path::Path;
 
-pub fn playlist_add(config: &Config, playlist_name: &str, song_paths: &Vec<&Path>) {
+pub fn playlist_add(_config: &Config, _playlist_name: &str, _song_paths: &Vec<&Path>) {
     unimplemented!()
 }
