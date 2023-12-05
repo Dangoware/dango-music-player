05fbb5868bf2c743cc649894dd34db8a532bb0a1
G2-Games <ke0bhogsg@gmail.com>
2023-12-04 21:09:10 -0600

    Fixed "about to finish" message sending improperly

--- dango-core/src/music_player.rs
+++ dango-core/src/music_player.rs
@@ -2,8 +2,9 @@
 //use crate::music_controller::config::Config;
 use crate::music_storage::music_db::URI;
 use std::error::Error;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, RwLock};
 use std::sync::mpsc::{self, Receiver, Sender};
+use crossbeam_channel::bounded;
 
 // GStreamer things
 use glib::{FlagsClass, MainContext};
@@ -26,14 +27,20 @@ pub enum PlayerCmd {
 pub struct Player {
     source: Option<URI>,
     //pub message_tx: Sender<PlayerCmd>,
-    pub message_rx: Receiver<PlayerCmd>,
-    playbin: Element,
+    pub message_rx: crossbeam::channel::Receiver<PlayerCmd>,
+    playbin: Arc<RwLock<Element>>,
     paused: bool,
     volume: f64,
-    start: Option<Duration>,
-    end: Option<Duration>,
-    position: Option<Duration>,
+    start: Arc<RwLock<Option<Duration>>>,
+    end: Arc<RwLock<Option<Duration>>>,
+    pub position: Arc<RwLock<Option<Duration>>>,
     gapless: bool,
+}
+
+impl Default for Player {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl Player {
@@ -41,9 +48,11 @@ impl Player {
         // Initialize GStreamer
         gst::init().unwrap();
 
-        let playbin = gst::ElementFactory::make("playbin").build().unwrap();
+        let playbin_arc = Arc::new(RwLock::new(gst::ElementFactory::make("playbin3").build().unwrap()));
 
-        let flags = playbin.property_value("flags");
+        let playbin = playbin_arc.clone();
+
+        let flags = playbin.read().unwrap().property_value("flags");
         let flags_class = FlagsClass::with_type(flags.type_()).unwrap();
 
         // Set up the Playbin flags to only play audio
@@ -56,15 +65,61 @@ impl Player {
             .unset_by_nick("text")
             .build()
             .unwrap();
-        playbin.set_property_from_value("flags", &flags);
 
+        playbin.write().unwrap().set_property_from_value("flags", &flags);
 
-        let (message_tx, message_rx) = std::sync::mpsc::channel();
-        playbin.connect("about-to-finish", false, move |_| {
-                println!("test");
-                message_tx.send(PlayerCmd::AboutToFinish).unwrap();
-                None
-            });
+        let position = Arc::new(RwLock::new(None));
+        let start = Arc::new(RwLock::new(None));
+        let end: Arc<RwLock<Option<Duration>>> = Arc::new(RwLock::new(None));
+
+        let position_update = position.clone();
+        let start_update = start.clone();
+        let end_update = end.clone();
+        let (message_tx, message_rx) = bounded(1);
+        std::thread::spawn(move || {
+            loop {
+                let mut pos_temp = playbin_arc
+                    .read()
+                    .unwrap()
+                    .query_position::<ClockTime>()
+                    .map(|pos| Duration::nanoseconds(pos.nseconds() as i64));
+
+                if pos_temp.is_some()
+                    && start_update.read().unwrap().is_some()
+                    && end_update.read().unwrap().is_some()
+                {
+                    if let Some(time) = *start_update.read().unwrap() { pos_temp = Some(pos_temp.unwrap() - time) }
+
+                    let atf = end_update.read().unwrap().unwrap() - Duration::milliseconds(100);
+                    if pos_temp.unwrap() >= end_update.read().unwrap().unwrap() {
+                        message_tx.try_send(PlayerCmd::Eos).unwrap();
+                        playbin_arc
+                            .write()
+                            .unwrap()
+                            .set_state(gst::State::Ready)
+                            .expect("Unable to set the pipeline state");
+                        *start_update.write().unwrap() = None;
+                        *end_update.write().unwrap() = None;
+                    } else if pos_temp.unwrap() >= atf {
+                        match message_tx.try_send(PlayerCmd::AboutToFinish) {
+                            Ok(_) => println!("Sent ATF"),
+                            Err(err) => println!("{}", err),
+                        }
+                    }
+                }
+
+                *position_update.write().unwrap() = pos_temp;
+
+                std::thread::sleep(std::time::Duration::from_millis(50));
+            }
+        });
+
+        /*
+        playbin.read().unwrap().connect("about-to-finish", false, move |_| {
+            //message_tx.send(PlayerCmd::AboutToFinish).unwrap();
+            None
+        });
+        */
 
         let source = None;
         Self {
@@ -72,11 +127,11 @@ impl Player {
             playbin,
             message_rx,
             paused: false,
-            volume: 0.5,
+            volume: 1.0,
             gapless: false,
-            start: None,
-            end: None,
-            position: None,
+            start,
+            end,
+            position,
         }
     }
 
@@ -84,27 +139,48 @@ impl Player {
         &self.source
     }
 
-    pub fn enqueue_next(&mut self, next_track: URI) {
-        self.set_state(gst::State::Ready);
-
+    pub fn enqueue_next(&mut self, next_track: &URI) {
+        self.ready();
         self.set_source(next_track);
-
         self.play();
     }
 
     /// Set the playback URI
-    pub fn set_source(&mut self, source: URI) {
+    fn set_source(&mut self, source: &URI) {
         self.source = Some(source.clone());
         match source {
-            URI::Cue {start, ..} => {
-                self.playbin.set_property("uri", source.as_uri());
-                self.play();
-                while self.state() != gst::State::Playing {
-                    std::thread::sleep(std::time::Duration::from_millis(10));
+            URI::Cue {start, end, ..} => {
+                self.playbin.write().unwrap().set_property("uri", source.as_uri());
+
+                // Set the start and end positions of the CUE file
+                *self.start.write().unwrap() = Some(Duration::from_std(*start).unwrap());
+                *self.end.write().unwrap() = Some(Duration::from_std(*end).unwrap());
+
+                self.pause();
+
+                // Wait for it to be ready, and then move to the proper position
+                while self.playbin.read().unwrap().query_duration::<ClockTime>().is_none() {
+                    std::thread::sleep(std::time::Duration::from_millis(1));
                 };
-                self.seek_to(Duration::from_std(start).unwrap()).unwrap();
-            }
-            _ => self.playbin.set_property("uri", source.as_uri()),
+
+                self.seek_to(Duration::from_std(*start).unwrap()).unwrap();
+            },
+            _ => {
+                self.playbin.write().unwrap().set_property("uri", source.as_uri());
+
+                self.pause();
+
+                while self.playbin.read().unwrap().query_duration::<ClockTime>().is_none() {
+                    std::thread::sleep(std::time::Duration::from_millis(1));
+                };
+
+                *self.start.write().unwrap() = Some(Duration::seconds(0));
+                *self.end.write().unwrap() = self.playbin
+                    .read()
+                    .unwrap()
+                    .query_duration::<ClockTime>()
+                    .map(|pos| Duration::nanoseconds(pos.nseconds() as i64));
+            },
         }
     }
 
@@ -117,7 +193,7 @@ impl Player {
     /// Set volume of the internal playbin player, can be
     /// used to bypass the main volume control for seeking
     fn set_gstreamer_volume(&mut self, volume: f64) {
-        self.playbin.set_property("volume", volume)
+        self.playbin.write().unwrap().set_property("volume", volume)
     }
 
     /// Returns the current volume level, a float from 0 to 1
@@ -127,8 +203,14 @@ impl Player {
 
     fn set_state(&mut self, state: gst::State) {
         self.playbin
+            .write()
+            .unwrap()
             .set_state(state)
             .expect("Unable to set the pipeline state");
+    }
+
+    pub fn ready(&mut self) {
+        self.set_state(gst::State::Ready)
     }
 
     /// If the player is paused or stopped, starts playback
@@ -150,30 +232,30 @@ impl Player {
 
     /// Check if playback is paused
     pub fn is_paused(&mut self) -> bool {
-        self.playbin.current_state() == gst::State::Paused
+        self.playbin.read().unwrap().current_state() == gst::State::Paused
     }
 
     /// Get the current playback position of the player
     pub fn position(&mut self) -> Option<Duration> {
-        self.position = self
-            .playbin
-            .query_position::<ClockTime>()
-            .map(|pos| Duration::nanoseconds(pos.nseconds() as i64));
-        self.position
+        *self.position.read().unwrap()
     }
 
     /// Get the duration of the currently playing track
     pub fn duration(&mut self) -> Option<Duration> {
-        if self.end.is_some() && self.start.is_some() {
-            Some(self.end.unwrap() - self.start.unwrap())
+        if self.end.read().unwrap().is_some() && self.start.read().unwrap().is_some() {
+            Some(self.end.read().unwrap().unwrap() - self.start.read().unwrap().unwrap())
         } else {
-            None
+            self.playbin
+                .read()
+                .unwrap()
+                .query_duration::<ClockTime>()
+                .map(|pos| Duration::nanoseconds(pos.nseconds() as i64))
         }
     }
 
     /// Seek relative to the current position
     pub fn seek_by(&mut self, seek_amount: Duration) -> Result<(), Box<dyn Error>> {
-        let time_pos = match self.position() {
+        let time_pos = match *self.position.read().unwrap() {
             Some(pos) => pos,
             None => return Err("No position".into()),
         };
@@ -188,13 +270,15 @@ impl Player {
         let seek_pos_clock = ClockTime::from_useconds(target_pos.num_microseconds().unwrap() as u64);
         self.set_gstreamer_volume(0.0);
         self.playbin
+            .write()
+            .unwrap()
             .seek_simple(gst::SeekFlags::FLUSH, seek_pos_clock)?;
         self.set_gstreamer_volume(self.volume);
         Ok(())
     }
 
     pub fn state(&mut self) -> gst::State {
-        self.playbin.current_state()
+        self.playbin.read().unwrap().current_state()
     }
 }
 
@@ -202,6 +286,8 @@ impl Drop for Player {
     /// Cleans up `GStreamer` pipeline when `Backend` is dropped.
     fn drop(&mut self) {
         self.playbin
+            .write()
+            .unwrap()
             .set_state(gst::State::Null)
             .expect("Unable to set the pipeline to the `Null` state");
     }
--- dango-music-player/src/main.rs
+++ dango-music-player/src/main.rs
@@ -1,24 +1,85 @@
+use std::sync::{Arc, RwLock};
+
+use chrono::Duration;
 use dango_core::{
-    music_storage::music_db::{URI, Service},
-    music_player::Player,
+    music_controller::config::Config,
+    music_storage::music_db::{MusicLibrary, Tag, Field},
+    music_player::{Player, PlayerCmd},
 };
 
 fn main() {
+    let config = Arc::new(RwLock::new(Config::default()));
+
+    let mut library = MusicLibrary::init(config).unwrap();
+
+    library.scan_folder("/media/g2/Storage4/Media-Files/Music/Albums/LiSA BEST -Day- & BEST -Way-/", &Config::default()).unwrap();
+
+    let query_results = library.query_tracks(
+        &"MATOKONOKO".to_string(),
+        &vec![Tag::Title],
+        &vec![Tag::Album,Tag::Disk,Tag::Track],
+    ).unwrap();
+
+    let albums = library.query_albums(
+        "LiVE is Smile Always ~LiTTLE DEVIL PARADE~ 2017.06.24 @SAITAMA SUPER ARENA"
+    ).unwrap();
+
     // Create a new player
     let mut player = Player::new();
-
-    // Add a stream to be queued
-    player.enqueue_next(
-        URI::Remote(
-            Service::InternetRadio,
-            "https://stream.gensokyoradio.net/3".to_string()
-        )
-    );
-
-    player.play();
     player.set_volume(0.4);
 
-    loop {
-        std::thread::sleep(std::time::Duration::from_millis(100));
+    let mut skip = 0;
+    for song in albums[0].tracks() {
+        if skip != 0 {
+            skip -= 1;
+            continue;
+        }
+
+        // Add a stream to be played
+        player.enqueue_next(&song.location);
+        if player.is_paused() {
+            player.play();
+        }
+
+        let location = match song.get_field("location").unwrap() {
+            Field::Location(loc) => loc.path(),
+            _ => std::path::PathBuf::new()
+        };
+
+        let loc_name = match location.file_name() {
+            Some(name) => name.to_str().unwrap(),
+            None => "",
+        };
+
+        println!("{} -- {}", song.get_tag(&Tag::Title).unwrap(), loc_name);
+
+        loop {
+            match player.message_rx.try_recv() {
+                Ok(msg) => match msg {
+                    PlayerCmd::AboutToFinish => {
+                        break
+                    },
+                    _ => ()
+                },
+                Err(_) => ()
+            }
+
+            match player.position() {
+                Some(pos) => print!(
+                    "{:2}:{:02}/{:2}:{:02} - {:?}\r",
+                    pos.num_minutes() % 60,
+                    pos.num_seconds() % 60,
+
+                    player.duration().unwrap_or(Duration::seconds(0)).num_minutes() % 60,
+                    player.duration().unwrap_or(Duration::seconds(0)).num_seconds() % 60,
+
+                    player.state()
+                ),
+                None => ()
+            }
+            std::io::Write::flush(&mut std::io::stdout()).unwrap();
+            std::thread::sleep(std::time::Duration::from_millis(100));
+        }
+        println!();
     }
 }
