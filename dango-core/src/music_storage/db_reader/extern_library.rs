88733027a8c58e0c004f1f62e14a45ca40d82d49
MrDulfin <Dulfinaminator@gmail.com>
2023-12-10 00:43:39 -0500

    added ``to_songs()`` in ExternalLibrary

--- dango-core/src/music_storage/db_reader/extern_library.rs
+++ dango-core/src/music_storage/db_reader/extern_library.rs
@@ -1,9 +1,12 @@
 use std::path::PathBuf;
 
+use crate::music_storage::library::Song;
+
 
 pub trait ExternalLibrary {
-    fn from_file(&mut self, file: &PathBuf) -> Self;
+    fn from_file(file: &PathBuf) -> Self;
     fn write(&self) {
         unimplemented!();
     }
+    fn to_songs(&self) -> Vec<Song>;
 }
