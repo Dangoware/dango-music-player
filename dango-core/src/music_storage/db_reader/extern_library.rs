71b67560f6cbdef3d5ec62b2fc6090d7cf755094
G2-Games <ke0bhogsg@gmail.com>
2023-12-09 23:39:15 -0600

    Updated `from_file()` in ExternalLibrary trait

--- dango-core/src/music_storage/db_reader/extern_library.rs
+++ dango-core/src/music_storage/db_reader/extern_library.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 
 pub trait ExternalLibrary {
-    fn from_file(&mut self, file: &PathBuf) -> Self;
+    fn from_file(file: &PathBuf) -> Self;
     fn write(&self) {
         unimplemented!();
     }
