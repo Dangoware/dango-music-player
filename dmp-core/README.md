a0c15a8b3049e4066e0340d345aac14c8b0af838
MrDulfin <dulfinaminator@gmail.com>
2024-12-07 16:54:40 -0500

    created rust workspace. merged dmp-core

--- dmp-core/README.md
+++
@@ -1,3 +0,0 @@
-# dango-core
-
-This is the backend crate for the [Dango Music Player](https://github.com/Dangoware/dango-music-player)
--- dmp-core/src/music_storage/music_collection.rs
+++
@@ -1,7 +0,0 @@
-use crate::music_storage::library::{AlbumArt, Song};
-
-pub trait MusicCollection {
-    fn title(&self) -> &String;
-    fn cover(&self) -> Option<&AlbumArt>;
-    fn tracks(&self) -> Vec<Song>;
-}
